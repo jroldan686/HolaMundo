36125a29a3d8d5c87aa7baf190b33c4ad047a0a8
jroldan686 <jroldan686@gmail.com>
2017-09-25 12:29:11 +0200

    Aplication HolaMundo

---
+++ app/src/main/java/com/example/usuario/holamundo/HolaMundoActivity.java
@@ -0,0 +1,27 @@
+package com.example.usuario.holamundo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+/**
+ *  Mi primera aplicación de Android
+ *  @author Jesús Roldán López
+ *  @version 1.0
+ *  @see android.app.Activity
+ */
+public class HolaMundoActivity extends AppCompatActivity {
+
+    private TextView txvMessage;
+
+    @Override
+    // Comentario de una línea
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hola_mundo);
+
+        txvMessage = (TextView) findViewById(R.id.txvMessage);
+        txvMessage.setText(R.string.second_message);
+    }
+}
+
